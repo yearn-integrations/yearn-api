c5f9c06c8089d5159567f57e3778c0bb87026a05
Soon Lai <soonlai814@gmail.com>
2020-11-30 16:35:42 +0800

    Update prod contract

--- config/serverless/domain.js
+++ config/serverless/domain.js
@@ -32,7 +32,7 @@ const devVaultContract = "0x99dd34943c741E17EB772041cd3D7E8d317FA92f";
 const devYfUSDTContract = "0x9680CF4CfED6Cf04eF0Eeb513c2399c192D0c0B0";
 const prodEarnContract = "0xe6354ed5bc4b393a5aad09f21c46e101e692d447";
 const prodVaultContract = "0x2f08119c6f07c006695e079aafc638b8789faf18";
-const prodYfUSDTContract = "0xE8c9F440677bDC8bA915734e6c7C1b232916877d";
+const prodYfUSDTContract = "0xA0db955B5bdFA7C279CdE6C136FBA20C195CdEe5";
 const aggregatedContractAddress = "0x9cad8ab10daa9af1a9d2b878541f41b697268eec";
 
 module.exports.devContract = {
