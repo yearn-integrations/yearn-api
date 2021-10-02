69938d317962f6b2fb2c464caf4b8bf5f4484c51
Phyllice Wai <waikahkwan0903@gmail.com>
2021-10-02 14:57:12 +0800

    APY and Historical APY.

--- models/historical-apy.model.js
+++ models/historical-apy.model.js
@@ -56,6 +56,7 @@ module.exports = {
   daoMPTFarmer: 'daoMPT_historical-apy',
   daoMVFFarmer: 'daoMVF_historical-apy',
   daoCDV2Farmer: 'daoCDV2_historical-apy',
+  daoTASFarmer: 'daoTAS_historical-apy',
   hfDaiFarmer: 'hfDAI_historical-apy',
   hfUsdcFarmer: 'hfUSDC_historical-apy',
   hfUsdtFarmer: 'hfUSDT_historical-apy',  
