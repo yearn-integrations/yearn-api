b961416422194d8d88e13a68af8a3052260c4a00
Phyllice Wai <waikahkwan0903@gmail.com>
2021-10-07 21:49:35 +0800

    Update vault symbol.

--- services/vaults/apy/save/bsc-vault.js
+++ services/vaults/apy/save/bsc-vault.js
@@ -59,7 +59,7 @@ const mainVaults = [
     name: "DAO Vault Degen",
     symbol: ["USDT", "USDC", "DAI"],
     description: "DAO Vault Degen for 3 stablecoins",
-    vaultSymbol: "daoSAFU",
+    vaultSymbol: "daoDEGEN",
     erc20address: [
       "0x55d398326f99059fF775485246999027B3197955",
       "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
